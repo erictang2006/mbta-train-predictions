db44044dabe5a9a56cafc90c8f526edefe54e64b
erictang2006 <erictang2006@gmail.com>
2025-01-30 00:41:31 -0500

    added other green line

--- src/config/routesConfig.js
+++ src/config/routesConfig.js
@@ -2,62 +2,34 @@ import GreenLineMap from "../assets/images/GreenLine.png";
 import RedLineMap from "../assets/images/RedLine.jpg";
 import OrangeLineMap from "../assets/images/OrangeLine.jpg";
 import GeneralMap from "../assets/images/GeneralMap.png";
+import { greenBStops, greenCStops, greenDStops, redLineStops, orangeLineStops } from "../stops/stops";
+
 
 const routesConfig = {
   "Green-B": {
     map: GreenLineMap,
-    color: "#3eed44", // Route color
-    stops: [
-        { stopId: "place-boyls", label: "Boylston" },
-        { stopId: "place-armnl", label: "Arlington" },
-        { stopId: "place-coecl", label: "Copley" },
-        { stopId: "place-hymnl", label: "Hynes Convention Center" },
-        { stopId: "place-kencl", label: "Kenmore Station - Green Line B" },
-        { stopId: "place-bland", label: "Blandford Street" },
-        { stopId: "place-buest", label: "Boston University East" },
-        { stopId: "place-bucen", label: "Boston University Central" },
-        { stopId: "place-amory", label: "Amory Street" },
-        { stopId: "place-babck", label: "Babcock Street" },
-        { stopId: "place-brico", label: "Packard's Corner" },
-        { stopId: "place-harvd", label: "Harvard Avenue" },
-    ],
+    color: "#32CD32",
+    stops: greenBStops,
+  },
+  "Green-C": {
+    map: GreenLineMap,
+    color: "#32CD32", // Slightly different green shade for Green-C
+    stops: greenCStops,
+  },
+  "Green-D": {
+    map: GreenLineMap,
+    color: "#228B22", // Another green variation for Green-D
+    stops: greenDStops,
   },
   "Red Line": {
-    map: GeneralMap,
-    color: "rgb(253, 80, 80)", // Route color
-    stops: [
-      { stopId: "place-alfcl", label: "Alewife" },
-      { stopId: "place-davis", label: "Davis" },
-      { stopId: "place-portr", label: "Porter" },
-      { stopId: "place-harsq", label: "Harvard Square" },
-      { stopId: "place-cntsq", label: "Central" },
-      { stopId: "place-knncl", label: "Kendall/MIT" },
-      { stopId: "place-chmnl", label: "Charles/MGH" },
-      { stopId: "place-pktrm", label: "Park Street" },
-      { stopId: "place-dwnxg", label: "Downtown Crossing" },
-      { stopId: "place-sstat", label: "South Station" },
-      { stopId: "place-brdwy", label: "Broadway" },
-      { stopId: "place-andrw", label: "Andrew" },
-      { stopId: "place-jfk", label: "JFK/UMass" },
-      { stopId: "place-shmnl", label: "Savin Hill" },
-      { stopId: "place-fldcr", label: "Fields Corner" },
-      { stopId: "place-smmnl", label: "Shawmut" },
-      { stopId: "place-asmnl", label: "Ashmont" },
-      { stopId: "place-nqncy", label: "North Quincy" },
-      { stopId: "place-wlsta", label: "Wollaston" },
-      { stopId: "place-qnctr", label: "Quincy Center" },
-      { stopId: "place-qamnl", label: "Quincy Adams" },
-      { stopId: "place-brntn", label: "Braintree" },
-    ],
+    map: RedLineMap,
+    color: "red",
+    stops: redLineStops,
   },
   "Orange Line": {
     map: OrangeLineMap,
-    color: "rgb(255, 187, 62)", // Route color
-    stops: [
-      { stopId: "place-ogmnl", label: "Oak Grove" },
-      { stopId: "place-mlmnl", label: "Malden Center" },
-      { stopId: "place-welln", label: "Wellington" },
-    ],
+    color: "orange",
+    stops: orangeLineStops,
   },
 };
 
---
+++ src/stops/stops.js
@@ -0,0 +1,83 @@
+const greenBStops = [
+    { stopId: "place-boyls", label: "Boylston" },
+    { stopId: "place-armnl", label: "Arlington" },
+    { stopId: "place-coecl", label: "Copley" },
+    { stopId: "place-hymnl", label: "Hynes Convention Center" },
+    { stopId: "place-kencl", label: "Kenmore" },
+    { stopId: "place-bland", label: "Blandford Street" },
+    { stopId: "place-buest", label: "Boston University East" },
+    { stopId: "place-bucen", label: "Boston University Central" },
+    { stopId: "place-amory", label: "Amory Street" },
+    { stopId: "place-babck", label: "Babcock Street" },
+    { stopId: "place-brico", label: "Packard's Corner" },
+    { stopId: "place-harvd", label: "Harvard Avenue" },
+    { stopId: "place-grigg", label: "Griggs Street" },
+    { stopId: "place-alsgr", label: "Allston Street" },
+    { stopId: "place-wrnst", label: "Warren Street" },
+    { stopId: "place-wascm", label: "Washington Street" },
+    { stopId: "place-sthld", label: "Sutherland Road" },
+    { stopId: "place-chswk", label: "Chiswick Road" },
+    { stopId: "place-chill", label: "Chestnut Hill Avenue" },
+    { stopId: "place-sougr", label: "South Street" },
+    { stopId: "place-bcnfd", label: "Boston College" },
+    ];
+
+    const greenCStops = [
+    { stopId: "place-cool", label: "Coolidge Corner" },
+    { stopId: "place-sntsn", label: "Saint Paul Street" },
+    { stopId: "place-kntst", label: "Kent Street" },
+    { stopId: "place-hwsst", label: "Hawes Street" },
+    { stopId: "place-smrst", label: "Summit Avenue" },
+    { stopId: "place-tapst", label: "Tappan Street" },
+    { stopId: "place-denrd", label: "Dean Road" },
+    { stopId: "place-engav", label: "Englewood Avenue" },
+    { stopId: "place-clmnl", label: "Cleveland Circle" },
+    ];
+
+    const greenDStops = [
+    { stopId: "place-fenwd", label: "Fenway" },
+    { stopId: "place-lngwd", label: "Longwood" },
+    { stopId: "place-bckhl", label: "Brookline Hills" },
+    { stopId: "place-bcnwa", label: "Beaconsfield" },
+    { stopId: "place-rsmnl", label: "Reservoir" },
+    { stopId: "place-chclt", label: "Chestnut Hill" },
+    { stopId: "place-newtn", label: "Newton Centre" },
+    { stopId: "place-newhl", label: "Newton Highlands" },
+    { stopId: "place-eliot", label: "Eliot" },
+    { stopId: "place-waban", label: "Waban" },
+    { stopId: "place-woodl", label: "Woodland" },
+    { stopId: "place-rvrst", label: "Riverside" },
+    ];
+
+    const redLineStops = [
+    { stopId: "place-alfcl", label: "Alewife" },
+    { stopId: "place-davis", label: "Davis" },
+    { stopId: "place-portr", label: "Porter" },
+    { stopId: "place-harsq", label: "Harvard Square" },
+    { stopId: "place-cntsq", label: "Central" },
+    { stopId: "place-knncl", label: "Kendall/MIT" },
+    { stopId: "place-chmnl", label: "Charles/MGH" },
+    { stopId: "place-pktrm", label: "Park Street" },
+    { stopId: "place-dwnxg", label: "Downtown Crossing" },
+    { stopId: "place-sstat", label: "South Station" },
+    { stopId: "place-brdwy", label: "Broadway" },
+    { stopId: "place-andrw", label: "Andrew" },
+    { stopId: "place-jfk", label: "JFK/UMass" },
+    { stopId: "place-shmnl", label: "Savin Hill" },
+    { stopId: "place-fldcr", label: "Fields Corner" },
+    { stopId: "place-smmnl", label: "Shawmut" },
+    { stopId: "place-asmnl", label: "Ashmont" },
+    { stopId: "place-nqncy", label: "North Quincy" },
+    { stopId: "place-wlsta", label: "Wollaston" },
+    { stopId: "place-qnctr", label: "Quincy Center" },
+    { stopId: "place-qamnl", label: "Quincy Adams" },
+    { stopId: "place-brntn", label: "Braintree" },
+    ];
+
+    const orangeLineStops = [
+    { stopId: "place-ogmnl", label: "Oak Grove" },
+    { stopId: "place-mlmnl", label: "Malden Center" },
+    { stopId: "place-welln", label: "Wellington" },
+    ];
+
+    export { greenBStops, greenCStops, greenDStops, redLineStops, orangeLineStops };
